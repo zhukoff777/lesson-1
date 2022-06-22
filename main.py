7a76d66043dfc65393841f0aae94afac61fffb5c
zhukoff777 <55098988+zhukoff777@users.noreply.github.com>
2022-06-22 19:47:44 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Zapis logarifma
+x = np.array([1, 10, 100, 1000])
+print("Значения x: \n", x)
+y = np.log(1 / np.e ** np.sin(x + 1) / (5 / 4 + 1 / x**15)) / np.log(1 + x**2)
+print(y)
+
+#Grafiki
+x = np.arange(-10, 10.01, 0.01)
+plt.plot(x, np.sin(x), x, np.cos(x), x, -x)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$f(x)$')
+plt.title(r'$f_1(x) = \sin(x),\ f_2(x) = \cos(x), f_3(x) = -x$')
+plt.grid(True)
+plt.show()
+
+
+
+
+
+
+
